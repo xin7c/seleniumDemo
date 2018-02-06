472d6ce0cbc3f7b78c868e4e0d9c831f1f44adcf
xuchu <xin7c@qq.com>
2018-02-06 14:50:48 +0800

    class

--- demo.py
+++ demo.py
@@ -3,25 +3,33 @@
 
 from selenium import webdriver
 import time
-browser = webdriver.Firefox()
-# 驱动文件位置 D:\Python27\geckodriver.exe
-browser.get('http://devtest.adinsights.cn:9000/admin/#/')
+class SeleniumDemo(object):
+	"""docstring for SeleniumDemo"""
+	def __init__(self):
+		super(SeleniumDemo, self).__init__()
+		# 驱动文件位置 D:\Python27\geckodriver.exe
+		self.browser = webdriver.Firefox()
 
-uname = browser.find_element_by_xpath("/html/body/div/div/div/div[2]/input[1]")
-upass = browser.find_element_by_xpath("/html/body/div/div/div/div[2]/input[2]")
-loginBtn = browser.find_element_by_xpath("/html/body/div/div/div/div[2]/div[3]")
+	def action(self):
+		self.browser.get('http://devtest.adinsights.cn:9000/admin/#/')
+		uname = self.browser.find_element_by_xpath("/html/body/div/div/div/div[2]/input[1]")
+		upass = self.browser.find_element_by_xpath("/html/body/div/div/div/div[2]/input[2]")
+		loginBtn = self.browser.find_element_by_xpath("/html/body/div/div/div/div[2]/div[3]")
 
-# login
-uname.send_keys("zhangliang@reyun.com")
-upass.send_keys("reyun123")
-loginBtn.click()
-print browser.title
-time.sleep(1)
-# 点击第一个审核
+		# login
+		uname.send_keys("zhangliang@reyun.com")
+		upass.send_keys("reyun123")
+		loginBtn.click()
+		print self.browser.title.encode('utf8')
+		time.sleep(1)
+		# 点击第一个审核
+		auditLinkOne = self.browser.find_element_by_xpath('//*[@id="app"]/div/div/div[1]/table/tr[2]/td[6]')
+		print auditLinkOne
+		auditLinkOne.click()
 
-auditLinkOne = browser.find_element_by_xpath('//*[@id="app"]/div/div/div[1]/table/tr[2]/td[6]')
-print auditLinkOne
-auditLinkOne.click()
+		time.sleep(5)
+		self.browser.close()
 
-time.sleep(5)
-browser.close()
+if __name__ == '__main__':
+	sd = SeleniumDemo()
+	sd.action()
