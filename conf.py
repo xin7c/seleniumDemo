06c36c78a9fe8bafa6029d24928ff10a958f5263
xuchu <xin7c@qq.com>
2018-03-21 15:13:08 +0800

    更新方法：获取tr列表

--- conf.py
+++ conf.py
@@ -5,8 +5,9 @@ conf = {
 			"uname": "/html/body/div/div/div[1]/div[2]/ul/li[1]/input",
 			"upass": "/html/body/div/div/div[1]/div[2]/ul/li[2]/input",
 			"loginBtn": "html/body/div/div/div[1]/div[2]/div[4]",
-			"selectData": "/html/body/div/div/div/div/div[1]/div/span",
-			"selectYesterday": "/html/body/div/div/div/div/div[1]/div/div/div[1]/a[1]"
+			"selectDate": "/html/body/div/div/div/div/div[1]/div/span",
+			"selectYesterday": "/html/body/div/div/div/div/div[1]/div/div/div[1]/a[1]",
+			"trXpathGame": "/html/body/div/div/div/div/div[2]/div[1]/table/tbody/tr[%s]/td[3]"
 		},
 		"sendKeys":{
 			"uname": "zhangliang@reyun.com",
--- demo.py
+++ demo.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 from selenium import webdriver
 import time
 from conf import conf
@@ -27,20 +30,33 @@ class SeleniumDemo(object):
 		uname.send_keys(conf["product"]["sendKeys"]["uname"])
 		upass.send_keys(conf["product"]["sendKeys"]["upass"])
 		loginBtn.click()
-		print(self.br.title.encode('utf8'))
+		print(self.br.title)
 		time.sleep(1)
 
 		# selectData
-		selectData = self.elXpath(conf["product"]["elements"]["selectData"])
+		selectData = self.elXpath(conf["product"]["elements"]["selectDate"])
 		selectData.click()
 		selectYesterday = self.elXpath(conf["product"]["elements"]["selectYesterday"])
 		selectYesterday.click()
 
 		# checkCretText
 		cretText = self.elXpath("/html/body/div/div/div/div/div[1]/div/span").text
-		print(cretText.encode('utf8'))
-		self.br.get_screenshot_as_file("xxx.png")
+		print(cretText)
 
+		# checkProductList
+		productTable_1 = self.br.find_element_by_xpath("/html/body/div/div/div/div/div[2]/div[1]/table/tbody")
+		# productTable_2 = productTable_1.find_elements_by_xpath("/tr")
+		# /html/body/div/div/div/div/div[2]/div[1]/table/tbody/tr[1]/td[3]
+		# /html/body/div/div/div/div/div[2]/div[1]/table/tbody/tr[2]/td[3]
+		productTable_2 = productTable_1.find_elements_by_tag_name("tr")
+		print(len(productTable_2))
+		for i, j  in enumerate(productTable_2):
+			trXpathGame = conf["product"]["elements"]["trXpathGame"] %(str(i+1))
+			print(self.elXpath(trXpathGame).text)
+			print("-" * 10)
+
+
+		self.br.get_screenshot_as_file("xxx.png")
 		time.sleep(1)
 		self.br.close()
 
