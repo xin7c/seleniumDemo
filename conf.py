c808ce1f389295b68dff3d8861f2d8e6863e8d62
xuchu <xin7c@qq.com>
2018-03-21 10:57:28 +0800

    fff

--- conf.py
+++ conf.py
@@ -1,7 +1,7 @@
 getUrl = "http://devtest.adinsights.cn:9000/#/product"
 
 conf = {
-	"product":{
+	"product": {
 		"elements":{
 			"uname": "/html/body/div/div/div[1]/div[2]/ul/li[1]/input",
 			"upass": "/html/body/div/div/div[1]/div[2]/ul/li[2]/input",
@@ -13,5 +13,8 @@ conf = {
 			"uname": "zhangliang@reyun.com",
 			"upass": "reyun123",
 		}
+	},
+	"admin": {
+
 	}
 }
