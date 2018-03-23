ef79035e393252fdad34e85d3265f5e576d0b833
xuchu <xin7c@qq.com>
2018-03-23 18:25:34 +0800

    pep8

--- conf.py
+++ conf.py
@@ -2,32 +2,32 @@
 # encoding: utf-8
 
 conf = {
-	"product": {
-		"getUrl": "http://devtest.adinsights.cn:9000/#/product",
-		"elements":{
-			"uname": "/html/body/div/div/div[1]/div[2]/ul/li[1]/input",
-			"upass": "/html/body/div/div/div[1]/div[2]/ul/li[2]/input",
-			"loginBtn": "html/body/div/div/div[1]/div[2]/div[4]",
-			"selectDate": "/html/body/div/div/div/div/div[1]/div/span",
-			"selectYesterday": "/html/body/div/div/div/div/div[1]/div/div/div[1]/a[1]",
-			"cretText": "/html/body/div/div/div/div/div[1]/div/span",
-			"productTableGame": "/html/body/div/div/div/div/div[2]/div[1]/table/tbody",
-			"trXpathGame": "/html/body/div/div/div/div/div[2]/div[1]/table/tbody/tr[%s]/td[3]",
-			"trXpathApp": "",
-			"trXpathBrand": ""
-		},
-		"sendKeys":{
-			"uname": "zhangliang@reyun.com",
-			"upass": "reyun123",
-		}
-	},
-	"admin": {
-		"elements":{
-			},
-		"sendKeys":{
-		}
-	},
-	"path":{
-		"screenShot":"D:\\xcgit\\seleniumDemo\\screenShot\\"
-	}
+    "product": {
+        "getUrl": "http://devtest.adinsights.cn:9000/#/product",
+        "elements": {
+            "uname": "/html/body/div/div/div[1]/div[2]/ul/li[1]/input",
+            "upass": "/html/body/div/div/div[1]/div[2]/ul/li[2]/input",
+            "loginBtn": "html/body/div/div/div[1]/div[2]/div[4]",
+            "selectDate": "/html/body/div/div/div/div/div[1]/div/span",
+            "selectYesterday": "/html/body/div/div/div/div/div[1]/div/div/div[1]/a[1]",
+            "cretText": "/html/body/div/div/div/div/div[1]/div/span",
+            "productTableGame": "/html/body/div/div/div/div/div[2]/div[1]/table/tbody",
+            "trXpathGame": "/html/body/div/div/div/div/div[2]/div[1]/table/tbody/tr[%s]/td[3]",
+            "trXpathApp": "",
+            "trXpathBrand": ""
+        },
+        "sendKeys": {
+            "uname": "zhangliang@reyun.com",
+            "upass": "reyun123",
+        }
+    },
+    "admin": {
+        "elements": {
+        },
+        "sendKeys": {
+        }
+    },
+    "path": {
+        "screenShot": "D:\\xcgit\\seleniumDemo\\screenShot\\"
+    }
 }
